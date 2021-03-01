02dc71a8750ddfe6d39187ba765e43452ace554b
Elronia <zar.catherine@gmail.com>
2021-02-28 23:31:43 -0500

    implementing .every method to check if everyone is 19 or older

--- ArrayMethods_Part2/script.js
+++ ArrayMethods_Part2/script.js
@@ -1,18 +1,18 @@
 // ## Array Cardio Day 2
 
 const people = [
-{ name: 'Wes', year: 1988 },
-{ name: 'Kait', year: 1986 },
-{ name: 'Irv', year: 1970 },
-{ name: 'Lux', year: 2015 }
+    { name: 'Wes', year: 1988 },
+    { name: 'Kait', year: 1986 },
+    { name: 'Irv', year: 1970 },
+    { name: 'Lux', year: 2015 }
 ];
 
 const comments = [
-{ text: 'Love this!', id: 523423 },
-{ text: 'Super good', id: 823423 },
-{ text: 'You are the best', id: 2039842 },
-{ text: 'Ramen is my fav food ever', id: 123523 },
-{ text: 'Nice Nice Nice!', id: 542328 }
+    { text: 'Love this!', id: 523423 },
+    { text: 'Super good', id: 823423 },
+    { text: 'You are the best', id: 2039842 },
+    { text: 'Ramen is my fav food ever', id: 123523 },
+    { text: 'Nice Nice Nice!', id: 542328 }
 ];
 
 // Some and Every Checks
@@ -35,6 +35,9 @@ const isAdult = people.some(person => (new Date()).getFullYear() - person.year >
 console.log({isAdult});
 
 // Array.prototype.every() // is everyone 19 or older?
+const allAdults = people.every(person => (new Date()).getFullYear() - person.year >= 19);
+
+console.log({allAdults});
 
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
